2bb364135a0a0625db289e5681b6f32844d4c800
Babich <nyutonyka@gmail.com>
2022-03-27 11:56:29 +0300

    Add do inputs via scanner

--- src/app/ProductInfo.java
+++ src/app/ProductInfo.java
@@ -1,12 +1,28 @@
 package app;
 
+import java.util.Scanner;
+
 public class ProductInfo {
     static String name;
     static double weight;
+    static int quantity;
     static Product product;
+    static private final String MEASURE = "кг";
 
     public static void main(String[] args) {
+        doInputs();
         handleData();
+    }
+
+    static private void doInputs() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Наименование товара: ");
+        name = scanner.nextLine();
+        System.out.print("Вес товара" + " (" + MEASURE + "): ");
+        weight = scanner.nextDouble();
+        System.out.print("Количество товара (шт.): ");
+        quantity = scanner.nextInt();
+        scanner.close();
     }
 
     static private void handleData() {
