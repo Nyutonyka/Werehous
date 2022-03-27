4a19f397d262ffb0a1a1c32eb4d42a0f70024c98
Babich <nyutonyka@gmail.com>
2022-03-27 11:49:03 +0300

    Build example Product

--- src/app/ProductInfo.java
+++ src/app/ProductInfo.java
@@ -1,8 +1,16 @@
 package app;
 
 public class ProductInfo {
+    static String name;
+    static double weight;
+    static Product product;
 
     public static void main(String[] args) {
+        handleData();
+    }
 
+    static private void handleData() {
+        product = new Product(name, weight);
+        name = product.getName();
     }
 }
